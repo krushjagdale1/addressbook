935009f959646ebf78ab941e6ff9acaede2fc761
Krushna Jagdale <149856043+krushjagdale1@users.noreply.github.com>
2024-06-03 20:39:40 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+         <H1><font color="white">Sample PHP application</font></H1>
+        <title> Hello Krushna Jagdale 
+          My First PHP Page</title>
+    </head>
+    <body>
+        <p>
+           <body bgcolor="#2C3E50">
+           <font color="white"> Hello. This is a sample PHP application!!</font>
+        </p>
+    </body>
+</html>
